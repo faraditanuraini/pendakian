9b68913b22d4fb5af3950a2324f96d40590daf52
nazrr <wildcookisfr@gmail.com>
2026-05-28 07:38:16 +0700

    update azza di branch nazrr

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,5 +11,12 @@ $routes->get('tiket', 'Tiket::index');
 $routes->get('sewa-alat', 'Home::sewaAlat');
 $routes->get('porter-guide', 'PorterGuide::index');
 $routes->get('porter-guide/(:num)', 'PorterGuide::index/$1');
+// Tambahkan kode ini di bawah rute porter-guide
+$routes->get('ojek', 'Ojek::index');
+$routes->get('ojek/(:num)', 'Ojek::index/$1');
 $routes->get('cek-booking', 'Booking::cek');
 $routes->get('gunung/detail/(:num)', 'Gunung::detail/$1');
+
+
+
+
